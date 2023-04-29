498bc60c3e145aebc0da7cb2830752818ee947d8
Kamilla <51264700+KamillaWilliam29@users.noreply.github.com>
2023-04-28 21:33:20 -0300

    Compose Docker
    Creepy Friday Progrmming class Adventures

---
+++ RestExample/src/main/java/br/com/senac/controller/EstudanteController.java
@@ -0,0 +1,47 @@
+package br.com.senac.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.senac.domain.Estudante;
+import br.com.senac.service.EstudanteService;
+
+@RestController
+@RequestMapping("estudante")
+public class EstudanteController {
+	private EstudanteService estudanteService;
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Estudante> buscarEstudantePorId(@PathVariable Long id){
+		return estudanteService.buscarEstudantePorId(id);
+	}
+	
+	@GetMapping("/listar")
+	public ResponseEntity<List<Estudante>> listarTodosEstudantes(){
+		return estudanteService.buscarTodosEstudantes();
+	}
+	
+	@PostMapping
+	public ResponseEntity<Estudante> cadastrarEstudante(@RequestBody Estudante estudante){
+		return estudanteService.cadastrarEstudante(estudante);
+	}
+	
+	@PostMapping
+	public ResponseEntity<Estudante> atualizarEstudante
+	(@RequestBody Estudante estudante){
+		return estudanteService.atualizarEstudante(estudante);
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<String> removerEstudante(@PathVariable Long id){
+		return estudanteService.removerEstudante(id);
+	}
+}
