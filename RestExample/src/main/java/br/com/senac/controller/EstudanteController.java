1e1f559656a640f81194b059a2b0e0b37351e5b6
Kamilla <51264700+KamillaWilliam29@users.noreply.github.com>
2023-05-02 16:54:50 -0300

    Update EstudanteController.java
    Comentários na classe Controller.

--- RestExample/src/main/java/br/com/senac/controller/EstudanteController.java
+++ RestExample/src/main/java/br/com/senac/controller/EstudanteController.java
@@ -1,7 +1,7 @@
 package br.com.senac.controller;
 
 import java.util.List;
-
+//Um dos métodos precisará de uma lista no retorno, portanto importaremos este objeto do Util.
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,14 +10,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+//Importamos aqui todos os objetos e notações necessárias para construir a nossa API Rest.
 
 import br.com.senac.domain.Estudante;
 import br.com.senac.service.EstudanteService;
+//Também importamos o nosso domain e o nosso service, como é de praxe no Controller.
 
 @RestController
 @RequestMapping("estudante")
 public class EstudanteController {
 	private EstudanteService estudanteService;
+	//Revisando: toda vez que temos um RequestMapping estaremos importando uma classe modelo para o nosso controller,
+	//a fim de podermos manipularmos os atributos delas. 
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<Estudante> buscarEstudantePorId(@PathVariable Long id){
@@ -28,6 +32,7 @@ public class EstudanteController {
 	public ResponseEntity<List<Estudante>> listarTodosEstudantes(){
 		return estudanteService.buscarTodosEstudantes();
 	}
+	//Como o nome destes métodos sugerem, vamos dar um Get ao terminar de chamarmos a classe service nos atributos indicados;
 	
 	@PostMapping
 	public ResponseEntity<Estudante> cadastrarEstudante(@RequestBody Estudante estudante){
@@ -39,9 +44,12 @@ public class EstudanteController {
 	(@RequestBody Estudante estudante){
 		return estudanteService.atualizarEstudante(estudante);
 	}
+	//Nos métodos de PostMapping, damos início ao que alteramos/cadastramos em primeiro lugar, lançando estas alterações em tempo de execução;
 	
 	@DeleteMapping("/{id}")
 	public ResponseEntity<String> removerEstudante(@PathVariable Long id){
 		return estudanteService.removerEstudante(id);
 	}
+	//Como é esperado, temos também um método DeleteMapping usado sempre que precisamos tirar algo que não saiu dentro do planejado.  Pelo id do
+	//ojeto, removemos da lista o que precisamos.
 }
